55aefde2a7b0729b85a23dc12ac62b41c3ce4b6b
Kevin Williams <kevin@k9w.org>
2022-02-22 08:07:13 +0000

    Initial commit.

---
+++ DoubleIt.cs
@@ -0,0 +1,12 @@
+using System;
+
+class DoubleIt
+{
+  static void Main()
+  {
+    Console.WriteLine("Give me a number and I will double it for you!");
+    int yourNumber = int.Parse(Console.ReadLine());
+    int yourDoubledNumber = yourNumber * 2;
+    Console.WriteLine("I doubled your number for you: " + yourDoubledNumber);
+  }
+}
